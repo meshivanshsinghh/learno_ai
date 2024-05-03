8f2a03890ceef6802e5b1a69cf6b415784580a30
Shivansh  Singh <shivanshsingh@Shivanshs-MacBook-Air.local>
2024-05-03 08:00:50 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "devco.ai",
+  title: "learno.ai",
   description: "Your AI-Powered Development Companion",
 };
 
